ee47889f29f3791f991c02be31ffd3886afb559c
Kevin Taylor <kevintaylor&84@gmail.com>
2023-03-30 16:06:59 -0400

    finshed with getting different categories

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,29 @@
+
+
+import ProductCard from '../../components/product-card/product-card.component';
+import { useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import './category.styles.scss';
+
+const Category = ()=>{
+  const {category }= useParams();
+  const {categoriesMap}= useContext(CategoriesContext);
+
+  const [products, setProducts ]= useState(categoriesMap[category]);
+
+useEffect(()=>{
+  setProducts(categoriesMap[category])
+},[categoriesMap,category])
+
+return (
+  <div className='category-container'>
+  {products &&
+    products.map((product)=><ProductCard  key={product.id} product={product}/>)
+  }
+  </div>
+);
+};
+
+export default Category;
